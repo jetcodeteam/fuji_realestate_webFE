d755740506d6d79d74200764a77c6ece5938cca5
Tran Duong <you@example.com>
2019-10-03 18:27:53 +0700

    [ADD] Add other services

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,6 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import homeIcon from '../static/images/icon/home-icon.png';
 import rentIcon from '../static/images/icon/rent-icon.png';
 import visaIcon from '../static/images/icon/visa-icon.png';
+import otherIcon from '../static/images/icon/other-icon.png';
 import avatar1 from '../static/images/avatar/avatar-1.jpeg';
 import avatar2 from '../static/images/avatar/avatar-2.jpg';
 
@@ -78,13 +79,40 @@ const HomePage = (props) => {
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
-      width: '100%',
+      alignSelf: 'center',
+      width: '80%',
       height: '60%',
       top: '100%',
     },
     serviceContent: {
       display: 'flex',
       width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    serviceIcon: {
+      width: '50px',
+      height: '50px',
+      marginBottom: '20px',
+    },
+    serviceWrapper: {
+      paddingLeft: '7%',
+      display: 'flex',
+      flexDirection: 'column',
+      flexGrow: 1,
+    },
+    serviceDescription: {
+      width: '100%',
+      fontSize: '14px',
+      opacity: 0.5,
+      marginBottom: '10%',
+    },
+    avatarArea: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexGrow: 1,
     },
   });
   const { t } = props;
@@ -110,71 +138,38 @@ const HomePage = (props) => {
         </div>
       </div>
       {/* ---------------- MAIN SERVICES ----------------- */}
-      <div className={classes.mainServices}>
-        <h2 style={{ flexGrow: 0.5 }}>{t('main_service')}</h2>
-        <div className={classes.serviceContent}>
-          <div
-            style={{
-              paddingLeft: '13%',
-              display: 'flex',
-              flexDirection: 'column',
-              flexGrow: 1,
-            }}
-          >
-            <img
-              style={{
-                width: '50px',
-                height: '50px',
-                marginBottom: '20px',
-              }}
-              src={homeIcon}
-              alt="Home"
-            />
-            <h3>{t('home_sales')}</h3>
-            <p style={{ width: '80%', fontSize: '14px', opacity: 0.5 }}>{t('home_sales_des')}</p>
-            <h4>{t('details')}</h4>
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              flexGrow: 1,
-              marginRight: '10%',
-              marginLeft: '10%',
-            }}
-          >
-            <img
-              style={{
-                width: '50px',
-                height: '50px',
-                marginBottom: '20px',
-              }}
-              src={visaIcon}
-              alt="Visa"
-            />
-            <h3>{t('visa_consulting')}</h3>
-            <p style={{ width: '80%', fontSize: '14px', opacity: 0.5 }}>{t('home_sales_des')}</p>
-            <h4>{t('details')}</h4>
-          </div>
-          <div
-            style={{
-              paddingRight: '10%',
-              display: 'flex',
-              flexDirection: 'column',
-            }}
-          >
-            <img
-              style={{
-                width: '50px',
-                height: '50px',
-                marginBottom: '20px',
-              }}
-              src={rentIcon}
-              alt="Rent"
-            />
-            <h3>{t('employment_consultation')}</h3>
-            <p style={{ width: '80%', fontSize: '14px', opacity: 0.5 }}>{t('home_sales_des')}</p>
-            <h4>{t('details')}</h4>
+      <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+        <div className={classes.mainServices}>
+          <h2 style={{ flexGrow: 0.5 }}>{t('main_service')}</h2>
+          <div className={classes.serviceContent}>
+            {/* home sales */}
+            <div className={classes.serviceWrapper}>
+              <img className={classes.serviceIcon} src={homeIcon} alt="Home" />
+              <h3 style={{ marginBottom: '7%' }}>{t('home_sales')}</h3>
+              <p className={classes.serviceDescription}>{t('home_sales_des')}</p>
+              <h4>{t('details')}</h4>
+            </div>
+            {/* visa consulting */}
+            <div className={classes.serviceWrapper}>
+              <img className={classes.serviceIcon} src={visaIcon} alt="Visa" />
+              <h3 style={{ marginBottom: '7%' }}>{t('visa_consulting')}</h3>
+              <p className={classes.serviceDescription}>{t('home_sales_des')}</p>
+              <h4>{t('details')}</h4>
+            </div>
+            {/* employment consultation */}
+            <div className={classes.serviceWrapper}>
+              <img className={classes.serviceIcon} src={rentIcon} alt="Rent" />
+              <h3 style={{ marginBottom: '7%' }}>{t('employment_consultation')}</h3>
+              <p className={classes.serviceDescription}>{t('home_sales_des')}</p>
+              <h4>{t('details')}</h4>
+            </div>
+            {/* other */}
+            <div className={classes.serviceWrapper}>
+              <img className={classes.serviceIcon} src={otherIcon} alt="Other" />
+              <h3 style={{ marginBottom: '7%' }}>{t('other')}</h3>
+              <p className={classes.serviceDescription}>{t('home_sales_des')}</p>
+              <h4>{t('details')}</h4>
+            </div>
           </div>
         </div>
       </div>
@@ -190,27 +185,11 @@ const HomePage = (props) => {
         </div>
         <div style={{ width: '50%' }}>
           <Grid container justify="center" alignItems="center">
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexGrow: 1,
-              }}
-            >
+            <div className={classes.avatarArea}>
               <Avatar alt="Remy Sharp" src={avatar1} className={classes.bigAvatar} />
               <div style={{ fontWeight: 700, textAlign: 'center' }}>幹事</div>
             </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexGrow: 1,
-              }}
-            >
+            <div className={classes.avatarArea}>
               <Avatar alt="Remy Sharp" src={avatar2} className={classes.bigAvatar} />
               <div style={{ fontWeight: 700, textAlign: 'center' }}>執行役員</div>
             </div>
